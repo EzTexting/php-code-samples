6874d05ca2e311973860214526517df229cb945d
Ez Texting <lyuba@eztexting.com>
2012-09-10 16:30:48 -0400

    cancel keyword using xml/json

---
+++ cancel_keyword_json.php
@@ -0,0 +1,35 @@
+<?php
+
+$data = array(
+    'User'     => 'winnie',
+    'Password' => 'the-pooh'
+);
+
+$curl = curl_init('https://app.eztexting.com/keywords/honey?format=json&_method=DELETE');
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// If you experience SSL issues, perhaps due to an outdated SSL cert
+// on your own server, try uncommenting the line below
+// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($curl);
+curl_close($curl);
+
+if ( !empty($response) ) {
+    $json = json_decode($response);
+    $json = $json->Response;
+
+    if ( 'Failure' == $json->Status ) {
+        $errors = array();
+        if ( !empty($json->Errors) ) {
+            $errors = $json->Errors;
+        }
+
+        echo 'Status: ' . $json->Status . "\n" .
+             'Errors: ' . implode(', ' , $errors) . "\n";
+    }
+} else {
+    echo 'Status: Success' . "\n";
+}
+
+?>
---
+++ cancel_keyword_xml.php
@@ -0,0 +1,33 @@
+<?php
+
+$data = array(
+    'User'     => 'winnie',
+    'Password' => 'the-pooh'
+);
+
+$curl = curl_init('https://app.eztexting.com/keywords/honey?format=xml&_method=DELETE');
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// If you experience SSL issues, perhaps due to an outdated SSL cert
+// on your own server, try uncommenting the line below
+// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($curl);
+curl_close($curl);
+
+if ( !empty($response) ) {
+    $xml = new SimpleXMLElement($response);
+    if ( 'Failure' == $xml->Status ) {
+        $errors = array();
+        foreach( $xml->Errors->children() as $error ) {
+            $errors[] = (string) $error;
+        }
+
+        echo 'Status: ' . $xml->Status . "\n" .
+             'Errors: ' . implode(', ' , $errors) . "\n";
+    }
+} else {
+    echo 'Status: Success' . "\n";
+}
+
+?>
